4e3482af47010ac8e5fbae317c123f2171679e5c
Vincent Boucheny <vincent.boucheny@powershop.co.nz>
2016-08-17 11:46:09 +1200

    The 'obfuscated' secret word is now given by a method in the Game model, and directly called from the view.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -6,22 +6,12 @@ class GamesController < ApplicationController
   def show
     @game = Game.find(params[:id])
     @game.errors.add(:base, flash[:notice]) if flash[:notice]
-    @secret_word = show_secret_word
   end
 
   def new
     @game = Game.new(secret: WordPicker.new.pick)
     @game.save
     redirect_to games_path
-  end
-
-  private
-
-  # Helper method in another file???
-  def show_secret_word
-    @game.secret.each_char.map do |char|
-      @game.tries.include?(char) ? char : '_'
-    end.join
   end
 end
 
--- app/models/game.rb
+++ app/models/game.rb
@@ -2,4 +2,10 @@ class Game < ApplicationRecord
   def lost?
     lives <= 0
   end
+
+  def show_secret_word
+    secret.each_char.map do |char|
+      tries.include?(char) ? char : '_'
+    end.join(' ')
+  end
 end
