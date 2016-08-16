b28b9e2f1d59c0ed6063841b6b11905ddf8de954
Vincent Boucheny <vincent.boucheny@powershop.co.nz>
2016-08-17 11:53:06 +1200

    Bad input handled.

--- app/controllers/tries_controller.rb
+++ app/controllers/tries_controller.rb
@@ -2,13 +2,17 @@ class TriesController < ApplicationController
   def new
     try = params[:guess]
     game = Game.find(params[:game_id])
-    if not game.tries.include?(try)
-      game.tries << try
-      game.lives -= 1 unless game.secret.include?(try)
-      game.update(game_params)
-      redirect_to game
+    if !(try =~ /^[[:alpha:]]$/)
+      redirect_to game, notice: "Bad input: '#{try}'"
     else
-      redirect_to game, notice: "Already tried #{try}"
+      if game.tries.include?(try)
+        redirect_to game, notice: "Already tried #{try}"
+      else
+        game.tries << try
+        game.lives -= 1 unless game.secret.include?(try)
+        game.update(game_params)
+        redirect_to game
+      end
     end
   end
 
