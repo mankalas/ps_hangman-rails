419f58a8429bd30349f4a7018f163de0488f46df
Vincent Boucheny <vincent.boucheny@powershop.co.nz>
2016-08-17 15:11:25 +1200

    Added validators in order to validate input and some game rules.

--- app/controllers/tries_controller.rb
+++ app/controllers/tries_controller.rb
@@ -2,17 +2,11 @@ class TriesController < ApplicationController
   def new
     try = params[:guess]
     game = Game.find(params[:game_id])
-    if !(try =~ /^[[:alpha:]]$/)
-      redirect_to game, notice: "Bad input: '#{try}'"
+    game.guess(try)
+    if game.update(game_params)
+      redirect_to game
     else
-      if game.tries.include?(try)
-        redirect_to game, notice: "Already tried #{try}"
-      else
-        game.tries << try
-        game.lives -= 1 unless game.secret.include?(try)
-        game.update(game_params)
-        redirect_to game
-      end
+      redirect_to game, notice: game.errors[:tries]
     end
   end
 
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,13 +1,33 @@
+class TryValidator < ActiveModel::Validator
+  def validate(record)
+    tries = record.tries
+    duplicates = tries.each_char.detect do |char|
+      tries.count(char) > 1
+    end
+    record.errors[:tries] << "Already tried '#{duplicates}'" if duplicates
+  end
+end
+
 class Game < ApplicationRecord
+  validates :secret,
+            presence: true,
+            format: { with: /\A[a-zA-Z]+\z/ }
+  validates :lives,
+            presence: true,
+            numericality: { only_integer: true,
+                            greater_than_or_equal_to: 0 }
+  validates :tries,
+            format: { with: /\A[a-zA-Z]*\z/, message: "input must be a letter" }
+
+  validates_with TryValidator, fields: [:tries]
+
   def lost?
     lives <= 0
   end
 
   def won?
-    if secret
-      secret.each_char.all? do |char|
-        tries.include?(char)
-      end
+    secret.each_char.all? do |char|
+      tries.include?(char)
     end
   end
 
@@ -15,5 +35,12 @@ class Game < ApplicationRecord
     secret.each_char.map do |char|
       tries.include?(char) ? char : '_'
     end.join(' ')
+  end
+
+  def guess(try)
+    self.tries << try
+    if valid?
+      self.lives -= 1 unless secret.include?(try)
+    end
   end
 end
