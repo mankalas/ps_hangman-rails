c5bbce52ab9e9373d9748df8331214627e71ec03
Vincent Boucheny <vincent.boucheny@powershop.co.nz>
2016-08-22 13:19:16 +1200

    Tidied up code.

--- spec/controllers/tries_controller_spec.rb
+++ spec/controllers/tries_controller_spec.rb
@@ -4,17 +4,48 @@ RSpec.describe TriesController, type: :controller do
   describe "GET #new" do
     fixtures :games
     let(:game) { games(:game) }
+    let(:letter) { 'e' }
+    # TODOL POST
+
+    def guess(input)
+      get :new, params: { guess: input, game_id: game.id }
+    end
+
+    describe "interaction with the game" do
+      before do
+        expect(Game).to receive(:find).and_return(game)
+      end
+
+      after do
+        guess(letter)
+      end
+
+      it "sends the input to the game" do
+        expect(game).to receive(:guess).with(letter).once
+      end
+
+      it "updates the game" do
+        expect(game).to receive(:update).once.with(any_args)
+      end
+    end
 
     context "guess is good" do
+      before do
+        guess(letter)
+      end
+
       it "redirects to the game's page" do
-        get :new, params: { guess: 'e', game_id: game.id }
         expect(response).to redirect_to game
+      end
+
+      it "redirects without any error" do
+        expect(flash[:notice]).to eq nil
       end
     end
 
     context "guess is bad input" do
       before do
-        get :new, params: { guess: '!', game_id: game.id }
+        guess('!')
       end
 
       it "redirects to the game's page" do
@@ -26,12 +57,10 @@ RSpec.describe TriesController, type: :controller do
       end
     end
 
-      # shared example???
+    # shared example???
     context "guess has already been made" do
-      let(:letter) { 'e' }
-
       before do
-        2.times { get :new, params: { guess: letter, game_id: game.id } }
+        2.times { guess(letter) }
       end
 
       it "redirects to the game's page" do
